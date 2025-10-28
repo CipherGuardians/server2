aef3ed3ea18539642cc793bf3282e7c394f2665f
CipherGuardians <91021781+CipherGuardians@users.noreply.github.com>
2025-10-28 14:10:33 +0100

    Update setup-khabarovsk.sh

--- setup-khabarovsk.sh
+++ setup-khabarovsk.sh
@@ -21,7 +21,7 @@ cat > /etc/sing-box/config.json <<EOF
       "listen": "0.0.0.0",
       "listen_port": 2443,
       "method": "2022-blake3-aes-256-gcm",
-      "password": "AtmtvGoTMcfTfmMgQnrF/SQa6fGTDMMBvIb431DpOMk=",
+      "password": "xAYjIaKCAGJhrDV9VM8XyOGStn80lt0h4Q8jsXhvV2I=",
       "sniff": false
     }
   ],
@@ -29,10 +29,10 @@ cat > /etc/sing-box/config.json <<EOF
     {
       "type": "shadowsocks",
       "tag": "out-ss",
-      "server":"198.13.42.196",
+      "server":"108.160.138.102",
       "server_port": 3443,
       "method": "2022-blake3-aes-256-gcm",
-      "password": "AtmtvGoTMcfTfmMgQnrF/SQa6fGTDMMBvIb431DpOMk="
+      "password": "xAYjIaKCAGJhrDV9VM8XyOGStn80lt0h4Q8jsXhvV2I="
     }
   ]
 }
